b1b554cbf6ccf7ec92dbbbd67d26e83a7890784c
John Lin <johnlin@mcjel.com>
2021-03-19 08:34:53 -0700

    Copy  func def from the headerfile and add debug printf... AddEvent implemented...

--- sequencer.cpp
+++ sequencer.cpp
@@ -14,6 +14,10 @@
 
 #include "sequencer.h"
 
+// DEBUG FLAG (to be removed in final stage)
+// should be in the headerfile, but the headerfile is asked not to be modified...
+#define DEBUG
+
 Sequencer::Sequencer() {
 
 }
@@ -29,21 +33,46 @@ void Sequencer::Init_() {
     callbackParamsLen_ = 0;
 }
 
+/// Register a sequence and callback to be invoked when the sequence has
+/// been matched.  If memory was allocated for the params, the caller is
+/// responsible for freeing that memory.
+/// @param sequence to match
+/// @param number of elements in sequence
+/// @param cb function pointer to callback
+/// @param params extra data to be passed in callback from caller
+/// @param params_len length of extra data passed in callback from caller
+/// @return true if sequence valid, false otherwise
 bool Sequencer::RegisterSequence(SequencerButtons_t *seq, uint8_t numElem, SequencerCb_Fn cb,
-                                 uint8_t *params, uint32_t paramsLen) {
-    // XXX IMPLEMENT ME - see sequencer.h for description
+                                 uint8_t *params, uint32_t paramsLen) 
+{
+    #ifdef DEBUG
+        for (int i; i < numElem; i++) {
+            printf("{%i}", seq[i]);
+        }
+    #endif
 
 }
 
+/// Unregister a sequence and callback.
 void Sequencer::UnregisterSequence() {
     // XXX IMPLEMENT ME - see sequencer.h for description
 
 
 }
 
+/// Add a button event.  This method is invoked each time an event (such as a button press) occurs.
+/// @param The new button event
 void Sequencer::AddEvent(SequencerButtons_t event) {
-    // XXX IMPLEMENT ME - see sequencer.h
+    #ifdef DEBUG
+        printf("[%i]", event);
+    #endif
 
+    if (sequenceIdx_ < SEQUENCE_MAX_LEN) {
+        sequence_[sequenceIdx_] = event;
+        sequenceIdx_++;
+    } else {
+        printf("ERROR: Sequence Length Too Long!");
+    }
 }
 
 /// @}
